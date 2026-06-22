4a2cb6b169178a77348e0b43bfee5b7626679b82
50shadescode <mnamdi89@gmail.com>
2026-06-22 14:21:07 +0300

    added the Casestudy page to main page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import Hero from "../components/Hero";
 import BentoGrid from "../components/BentoGrid";
-import ProcessTimeline from "../components/ProcessTimeline";
+import CaseStudies from "../components/CaseStudies"; // Updated import
 import StudioDNA from "../components/StudioDNA";
 import ContactFooter from "../components/ContactFooter";
 
@@ -9,7 +9,7 @@ export default function HomePage() {
     <main className="min-h-screen bg-[#030712]">
       <Hero />
       <BentoGrid />
-      <ProcessTimeline />
+      <CaseStudies /> {/* Swapped ProcessTimeline out for your new Case Studies */}
       <StudioDNA />
       <ContactFooter />
     </main>
